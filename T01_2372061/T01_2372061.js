e1faf62b7526d0885d219537a7d09fa9ffa002de
lauraa15 <151803275+lauraa15@users.noreply.github.com>
2025-09-17 07:56:14 +0700

    idk

--- T01_2372061/T01_2372061.js
+++ T01_2372061/T01_2372061.js
@@ -3,24 +3,26 @@
 // 1. create canvas
 var cnv;
 cnv = document.querySelector("#main-canvas");
-var atas,bawah,kanan,kiri;
+
+var atas,bawah,kanan,kiri,clear;
 atas=document.getElementById("atas");
 bawah=document.getElementById("bawah");
 kanan=document.getElementById("kanan");
 kiri=document.getElementById("kiri");
-// console.log(cnv,atas,bawah,kiri,kanan);
+clear=document.getElementById("clear");
 
-// 1.5. Bikin function buat gambar garis
-function gambar_titik(imageData,x,y,color){
+// 1.5. Bikin function buat gambar titik
+function gambar_titik(imageData,x,y,r,g,b){
     // rumus::: index = 4 * (x +(y * image_width)) 
     var index;
-    index = 4 * (x +(y * cnv.width)) 
+    index = 4 * (x +(y * cnv.width)) ;
 
-    imageData.data[index]  =color.r;//Red
-    imageData.data[index+1]=color.g;// Green
-    imageData.data[index+2]=color.b;// Blue
+    imageData.data[index]  =r;//Red
+    imageData.data[index+1]=g;// Green
+    imageData.data[index+2]=b;// Blue
     imageData.data[index+3]=255;// Alpha / transparency
 }
+
 
 // 2. create context
 var context1;
@@ -29,23 +31,26 @@ context1 = cnv.getContext("2d");
 // 3. image datanya
 var imageData = context1.getImageData(0,0,cnv.width,cnv.height);
 
-for (let j = 0; j < 100; j++) {
-    for (var i = 0; i < 100; i++) {
-        let color = {r:255,g:0,b:100+j}
-        gambar_titik(imageData,100+i,100+j, color);
-    }    
+function functionAtas(){
+    gambar_titik(imageData,50,50,255,0,0);
+}
+function functionBawah(){
+    gambar_titik(imageData,400,400,255,0,0);
+}
+function functionKanan(){
+    gambar_titik(imageData,400,50,255,0,0);
+}
+function functionKiri(){
+    gambar_titik(imageData,50,400,255,0,0);
+}
+function clearing(){
+    cnv.width = cnv.width;;
 }
 
-for (var i = 0; i < 100; i++) {
-    // gambar_titik(imageData,100+i,100,255,0,0);
-    // gambar_titik(imageData,100,100+i,255,0,0);
-    // gambar_titik(imageData,i,i,255,0,0);
-    // gambar_titik(imageData,100+i,100,255,0,0);
-    // gambar_titik(imageData,100,100+i,255,0,0);
-    // gambar_titik(imageData,100+i,200,0,0,255);
-    // gambar_titik(imageData,200,100+i,0,0,255);
-    
-}
-
+atas.addEventListener("click",functionAtas())
+bawah.addEventListener("click",functionBawah())
+kanan.addEventListener("click",functionKanan())
+kiri.addEventListener("click",functionKiri())
+clear.addEventListener("click",clearing())
 // naro grafis ke context ke canvas
 context1.putImageData(imageData,0,0);
