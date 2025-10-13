9029c66d40180568ef3626bac9ec68eeba59bdfb
lauraa15 <151803275+lauraa15@users.noreply.github.com>
2025-10-13 23:19:55 +0700

    prak4-soal2

---
+++ P04_2372061/Soal2/Soal2.js
@@ -0,0 +1,110 @@
+function titik(imageData, x, y, r,g,b){
+    var index = 4*(Math.ceil(x)+(Math.ceil(y)*cnv.width));
+    imageData.data[index]   = r;
+    imageData.data[index+1] = g;
+    imageData.data[index+2] = b;
+    imageData.data[index+3] = 255;
+}
+
+
+function garis_dda(imageData, x1,y1, x2,y2, r,g,b){
+    var dx=x2-x1;
+    var dy=y2-y1;
+
+    if(Math.abs(dx)>Math.abs(dy)){
+        //bikin garis di sumbu x
+        if(x2 > x1){
+            // jalan ke kanan
+            var y=y1;
+            for(var x=x1; x<x2;x++){
+                y = y + dy/Math.abs(dx)   // 1/m
+                titik(imageData, x, y, r,g,b)
+            }
+        } else { 
+            // jalan ke kiri
+            var y=y1;
+            for(var x=x1; x>x2;x--){
+                y = y + dy/Math.abs(dx)   // 1/m
+                titik(imageData, x, y, r,g,b)
+            }
+        }
+
+    } else { // bikin garis di sb y
+        if(y2 > y1){
+            // jalan ke kanan
+            var x=x1;
+            for(var y=y1; y<y2;y++){
+                x = x + dx/Math.abs(dy)   // m
+                titik(imageData, x, y, r,g,b)
+            }
+        } else { 
+            // jalan ke kiri
+            var x=x1;
+            for(var y=y1; y>y2;y--){
+                x = x + dx/Math.abs(dy)   // m
+                titik(imageData, x, y, r,g,b)
+            }
+        }
+
+    }
+}
+
+function floodfill(imageData,cnv, x, y,  toFlood, color){
+    var index = 4*(Math.ceil(x)+(Math.ceil(y)*cnv.width));
+    var r1 = imageData.data[index];
+    var g1 = imageData.data[index+1];
+    var b1 = imageData.data[index+2];
+
+    if((toFlood.r == r1) && (toFlood.g == g1) && (toFlood.b == b1)){
+        imageData.data[index]   = color.r;
+        imageData.data[index+1] = color.g;
+        imageData.data[index+2] = color.b;
+        imageData.data[index+3] = 255;
+
+        floodfill(imageData,cnv, x+1, y,  toFlood, color);
+        floodfill(imageData,cnv, x, y+1,  toFlood, color);
+        
+        floodfill(imageData,cnv, x-1, y,  toFlood, color);
+        floodfill(imageData,cnv, x, y-1,  toFlood, color);
+    }
+}
+
+function floodfillStack(imageData,cnv, x0, y0,  toFlood, color){
+    var index = 4*(x0+(y0*cnv.width));
+    var r1 = imageData.data[index];
+    var g1 = imageData.data[index+1];
+    var b1 = imageData.data[index+2];
+
+    var tumpukan =[];
+    tumpukan.push({x:x0, y:y0});
+    // inisialisasi awal
+
+    while(tumpukan.length>0){
+        var titikS = tumpukan.pop();
+        var indexS = 4*(Math.ceil(titikS.x)+(Math.ceil(titikS.y)*cnv.width));
+        var r1 = imageData.data[indexS];
+        var g1 = imageData.data[indexS+1];
+        var b1 = imageData.data[indexS+2];
+
+        
+        if((toFlood.r == r1) && (toFlood.g == g1) && (toFlood.b == b1)){
+            imageData.data[indexS]   = color.r;
+            imageData.data[indexS+1] = color.g;
+            imageData.data[indexS+2] = color.b;
+            imageData.data[indexS+3] = 255;
+            tumpukan.push({x:titikS.x+1, y: titikS.y});
+            tumpukan.push({x:titikS.x-1, y: titikS.y});
+            tumpukan.push({x:titikS.x, y: titikS.y+1});
+            tumpukan.push({x:titikS.x, y: titikS.y-1});
+        }
+    }
+}
+
+function bunga(imageData, xc, yc, rad, n, ra,ga,ba){
+    for(var theta=0; theta<=Math.PI*2; theta+=0.0001){
+        var x = xc + (rad * Math.cos(theta)*Math.cos(n*theta));
+        var y = yc + (rad*Math.cos(n*theta) * Math.sin(theta));
+        titik(imageData, x,y,ra,ga,ba);
+    }   
+    // for(){}
+}
